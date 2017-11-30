7f0bc95a0ddd9a7e21aa272fece823690ccd4030
Abdul Rahman Khan <rahman.khan803@gmail.com>
2017-11-30 19:26:00 +0500

    6th

--- app/src/main/java/com/example/cracker/gameboard/Check.java
+++ app/src/main/java/com/example/cracker/gameboard/Check.java
@@ -1,40 +1,77 @@
 package com.example.cracker.gameboard;
 
-import android.graphics.drawable.Drawable;
+import android.app.Activity;
+import android.content.Context;
 import android.widget.ImageView;
-
-import static android.support.v4.media.session.MediaControllerCompatApi21.getPackageName;
 
 /**
  * Created by cracker on 11/20/2017.
  */
 
 public class Check {
-    boolean turn ;
-    ImageView imageView =null;
-    Check(boolean turn, ImageView imageView){
-        this.turn = turn;
+    Activity context;
+    ImageView imageView = null;
+
+    Check(ImageView imageView, Activity context) {
         this.imageView = imageView;
+        this.context = context;
     }
-    public void check(){
-        if(turn == true){
-            if (imageView.getTag().equals("pawn"))
-            {
-                String s = String.valueOf(imageView.getId());
-                String l[] = s.split("\\.");
-                String str  = (l[l.length-1]);
-                int id = Integer.parseInt(str.substring(9));
-                Drawable drawble = imageView.getResources().getIdentifier("imageView"+(id+7),"drawable",getPackageName());
 
-            }else if(imageView.getTag().equals("queen")){
+    public void check() {
+        if (imageView.getTag().equals("pawnP1")) {
+            String s = String.valueOf(imageView.getId());
+            String l[] = s.split("\\.");
+            String str = (l[l.length - 1]);
+            int id = Integer.parseInt(str.substring(9));
+            checkFront(id,7,9);
+            int left = imageView.getResources().getIdentifier("imageView" + (id + 7), "drawable", context.getPackageName());
 
+
+        }else if (imageView.getTag().equals("queenP1")){
+
+        }else if (imageView.getTag().equals("pawnP2")){
+
+        }else if (imageView.getTag().equals("queenP2")){
+
+        }
+    }
+
+    public void transition(int first,int second){
+
+
+    }
+
+    public void setMovable(ImageView image){
+//        Resources res = context.getResources();
+//        final ImageView image = (ImageView) findViewById(R.id.imageId);
+//        final int newColor = res.getColor(R.color.new_color);
+//        image.setColorFilter(newColor, Mode.SRC_ATOP);
+
+    }
+
+
+    public void checkFront(int id, int left, int right ){
+        if ( (id+left)>0 && (id+left)<64   ){
+           left = context.getResources().getIdentifier("imageView"+(id+left),"drawable",context.getPackageName());
+           ImageView imleft = (ImageView) context.findViewById(left);
+            if (imleft.getTag().equals("void")){
+                setMovable(imleft);
+            }
+            if (imageView.getTag()!=imleft.getTag() && !imleft.getTag().equals("void")){
+                
 
             }
-        }
-        else {
 
         }
+
 
 
     }
+    public void checkBack(){
+
+    }
+    public void checkHorizontal(){
+
+    }
+
 }
